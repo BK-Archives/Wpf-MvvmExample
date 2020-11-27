8b404ba78b5a69fecb424a8e2ec714ef65d57dc4
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-11-27 12:58:49 +0100

    added tests for mainviewmodel by mocking userinteraction

---
+++ MvvmExample.Logic.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MvvmExample.DataAccess;
+using MvvmExample.Logic.UserInteraction;
+using MvvmExample.Logic.ViewModels;
+
+namespace MvvmExample.Logic.Tests
+{
+	[TestClass]
+	public class MainWindowViewModelTests
+	{
+		[TestMethod]
+		public void ClearItemsCommand_WhenUserInteraction_ReturnsYes_ItemsShouldBeCleared()
+		{
+			// arrange
+			var vm = Setup(UserInteractionResult.Yes);
+			vm.LoadItemsCommand.Execute(null);
+
+			// act
+			vm.ClearItemsCommand.Execute(null);
+
+			// assert
+			Assert.IsTrue(vm.Items.Count == 0);
+		}
+
+		[TestMethod]
+		public void ClearItemsCommand_WhenUserInteraction_ReturnsNo_ItemsShouldNotBeCleared()
+		{
+			// arrange
+			var vm = Setup(UserInteractionResult.No);
+			vm.LoadItemsCommand.Execute(null);
+			var items = vm.Items;
+
+			// act
+			vm.ClearItemsCommand.Execute(null);
+
+			// assert
+			Assert.IsTrue(vm.Items.Count != 0 && vm.Items.Count == items.Count);
+			Assert.AreEqual(vm.Items, items);
+		}
+
+		private MainWindowViewModel Setup(UserInteractionResult userInteractionMockResult)
+		{
+			var itemRepository = new ItemRepository();
+			var userInteraction = new UserInteractionMock(userInteractionMockResult);
+			var vm = new MainWindowViewModel(itemRepository, userInteraction);
+			return vm;
+		}
+	}
+
+	class UserInteractionMock : IUserInteraction
+	{
+		private readonly UserInteractionResult _result;
+
+		public UserInteractionMock(UserInteractionResult result) => _result = result;
+
+		public UserInteractionResult DisplayAlert(string title, string prompt,
+			UserInteractionOption option = UserInteractionOption.YesNo)
+		{
+			return _result;
+		}
+	}
+}
---
+++ MvvmExample.Logic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvvmExample.Logic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvvmExample.Logic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63106d41-d803-4f16-892b-d2750f7bef2a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
